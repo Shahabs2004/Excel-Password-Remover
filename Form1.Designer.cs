ab3d5525f2e845a6d425dba9b2e2b9e36cf3b522
Shahabs2004 <Shahabs2004@Gmail.com>
2024-05-29 08:59:20 +0330

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace ExcelPasswordRemover
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.LogBox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Enabled = false;
+            this.btnOpenFile.Location = new System.Drawing.Point(86, 12);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(120, 23);
+            this.btnOpenFile.TabIndex = 0;
+            this.btnOpenFile.Text = "Open Protected File";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Visible = false;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // LogBox
+            // 
+            this.LogBox.FormattingEnabled = true;
+            this.LogBox.Location = new System.Drawing.Point(6, 42);
+            this.LogBox.Name = "LogBox";
+            this.LogBox.Size = new System.Drawing.Size(282, 186);
+            this.LogBox.TabIndex = 1;
+            this.LogBox.SelectedIndexChanged += new System.EventHandler(this.LogBox_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 231);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Code by: Shahab Sadeghi";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(293, 250);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LogBox);
+            this.Controls.Add(this.btnOpenFile);
+            this.Name = "MainForm";
+            this.Text = "Excel Password Remover";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.ListBox LogBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Xml;
+using System.IO;
+using System.IO.Compression;
+using System.Windows.Forms;
+using System.Globalization;
+
+
+namespace ExcelPasswordRemover
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        public void RemoveTagFromFile(string fileName,string NodeName)
+        {
+            try
+            {
+                XmlDocument xmlDocument = new XmlDocument();
+                xmlDocument.Load(fileName);
+
+                XmlNamespaceManager namespaceManager = new XmlNamespaceManager(xmlDocument.NameTable);
+                namespaceManager.AddNamespace("ns", "http://schemas.openxmlformats.org/spreadsheetml/2006/main");
+
+                XmlNodeList sheetProtectionNodes = xmlDocument.SelectNodes("//ns:"+NodeName, namespaceManager);
+
+                foreach (XmlNode node in sheetProtectionNodes)
+                {
+                    node.ParentNode.RemoveChild(node);
+                }
+
+                xmlDocument.Save(fileName);
+                LogBox.Items.Add("Remove Protection From " + Path.GetFileNameWithoutExtension(fileName));
+            }
+            catch (Exception ex)
+            {
+                LogBox.Items.Add("Error occurred while removing Protection from Sheet: " + ex.Message);
+            }
+        }
+        public void ExtractZipFile(string zipFilePath)
+        {
+            string tempFolderPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Temp");
+            try
+            {
+                if (Directory.Exists(tempFolderPath))
+                {
+                    Directory.Delete(tempFolderPath, true);
+                }
+                else
+                {
+                    Directory.CreateDirectory(tempFolderPath);
+                }
+                try
+                {
+                    ZipFile.ExtractToDirectory(zipFilePath, tempFolderPath);
+                    LogBox.Items.Add("Let See whats inside");
+
+                }
+                catch (Exception ex)
+                {
+                    LogBox.Items.Add("Can not see: " + ex.Message);
+                }
+            }
+            catch (Exception ex)
+            {
+                LogBox.Items.Add("dont mess with me: " + ex.Message);
+            }
+
+
+
+        }
+        public void CompressDirectory(string directoryPath)
+        {
+            string zipFilePath = string.Empty;
+
+            try
+            {
+                using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+                {
+                    saveFileDialog.Filter = "Excel File |*.xlsx";
+                    saveFileDialog.Title = "Save Excel File";
+                    saveFileDialog.CheckFileExists = false;
+
+                    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        zipFilePath = saveFileDialog.FileName;
+
+                        if (File.Exists(zipFilePath))
+                        {
+                            File.Delete(zipFilePath);
+                        }
+
+                        ZipFile.CreateFromDirectory(directoryPath, zipFilePath);
+                        LogBox.Items.Add("Well Done Boy :)");
+                    }
+                    else
+                    {
+                        LogBox.Items.Add("WTF...");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                LogBox.Items.Add("WTF: " + ex.Message);
+            }
+            /*            finally
+                        {
+                            if (!string.IsNullOrEmpty(zipFilePath) && File.Exists(zipFilePath))
+                            {
+                                // In case of any exception, delete the partially created compressed file
+                                File.Delete(zipFilePath);
+                            }
+                        }*/
+        }
+        public  void ProcessXmlSheetFiles(string directoryPath)
+        {
+            try
+            {
+                string[] xmlFiles = Directory.GetFiles(directoryPath, "*.xml");
+
+                foreach (string xmlFile in xmlFiles)
+                {
+                    Console.WriteLine($"Processing XML file: {xmlFile}");
+
+                    // Perform your desired operations on the XML file here
+                    // For example, you can call the RemoveTagFromFile function from the previous response
+
+                    // Uncomment the following line if you want to remove the 'sheetProtection' tag from each XML file
+                    RemoveTagFromFile(xmlFile, "sheetProtection");
+                }
+
+                Console.WriteLine("XML files processed successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error occurred while processing XML files: " + ex.Message);
+            }
+        }
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Excel Files|*.xlsx";
+            openFileDialog.Title = "Select an Excel File";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                LogBox.Items.Add("Opening...");
+                string selectedFilePath = openFileDialog.FileName;
+                LogBox.Items.Add("Going Deep...");
+                ExtractZipFile(selectedFilePath);
+                LogBox.Items.Add("I am inside");
+                try
+                {
+                    string sheetFilesPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Temp\\xl\\worksheets");
+                    LogBox.Items.Add("Try Remove Protection From Sheets");
+                    ProcessXmlSheetFiles(sheetFilesPath);
+                    LogBox.Items.Add("Sheets Finished");
+                    LogBox.Items.Add("Try remove Protection From Workbook");
+                    RemoveTagFromFile(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Temp\\xl\\workbook.xml"), "workbookProtection");
+                    LogBox.Items.Add("Finished");
+                    LogBox.Items.Add("ReGenerate Excel File...");
+                    CompressDirectory(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Temp"));
+
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+            string tempFolderPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Temp");
+            if (Directory.Exists(tempFolderPath))
+            {
+                Directory.Delete(tempFolderPath, true);
+            }
+
+        }
+        private void LogBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            LogBox.Items.Clear();
+                btnOpenFile.Enabled = true;
+                btnOpenFile.Visible = true;
+        }
+    }
+}
